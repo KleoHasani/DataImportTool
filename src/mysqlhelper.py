2d48e6f1eb961ea2174fadbe8be5d96c46e64161
Kleo <kleo@hasani>
2021-10-22 21:46:26 -0400

    added mysql connection

--- src/mysqlhelper.py
+++ src/mysqlhelper.py
@@ -1,23 +1,24 @@
 from os import environ
 from dotenv import load_dotenv
+from mysql.connector import connect, Error
 
 load_dotenv(".env")
 
-# ENV vars.
 DB_HOST=environ.get("DB_HOST")
+DB_PORT=environ.get("DB_PORT")
 DB_USER=environ.get("DB_USER")
 DB_PASSWORD=environ.get("DB_PASSWORD")
 DB_NAME=environ.get("DB_NAME")
 
-# Create new db connection and return connection.
-def connect():
-    return
-
-# Execute SQL on the passed db connection.
-def exec(conn):
-    return
-
-# Disconnect db from passed connection.
-def disconnect(conn):
-    return
-
+def exec(self, data):
+    try:
+        with connect(
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            user=self.DB_USER,
+            password=self.DB_PASSWORD,
+            database=self.DB_NAME
+        ) as connection:
+            print(connection)
+    except Error as e:
+        print(e)
