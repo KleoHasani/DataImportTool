33a6450502da515852e2bdcf12f34e1fe21cb460
Kleo Hasani <64975970+KleoHasani@users.noreply.github.com>
2021-12-15 08:39:00 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='DataImportTool',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(include=['src']),
-    install_requires=[],
+    install_requires=[
+        'Click'
+    ],
     entry_points={
         'console_scripts': [
             'dit = src.main:cli',
